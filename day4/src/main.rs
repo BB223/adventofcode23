2b6d925adb1af1d4fa37a36bc75df7fa3516b6dd
BB223 <79644542+BB223@users.noreply.github.com>
2024-08-04 19:30:42 +0200

    day4.2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,39 +1,34 @@
+use std::usize;
+
 use regex::Regex;
 
 #[tokio::main]
 async fn main() {
     let input = aoc_input_resolver::get_input(4).await;
     let lines = input.lines();
-    let mut points = 0;
-    let base: u32 = 2;
-    let regex = Regex::new(r"Card\s+\d+:\s+").unwrap();
-    for line in lines {
+    let mut multi = vec![1; lines.clone().count()];
+    let regex = Regex::new(r"Card\s+(\d+):\s+").unwrap();
+    for (i, line) in lines.into_iter().enumerate() {
         let clean_line = regex.replace(line, "");
         let inputs: Vec<&str> = clean_line.split("|").collect();
         let winning: Vec<i32> = inputs[0].split_whitespace().map(|number| number.parse().unwrap()).collect();
         let own: Vec<i32> = inputs[1].split_whitespace().map(|number| number.parse().unwrap()).collect();
         let exp = matcher(&winning, &own);
-        let p = match exp {
-            Some(n) => base.pow(n),
-            None => 0
-        };
-        points += p;
+        for n in i+1..=i+exp  {
+            multi[n] += multi[i];
+        }
     }
-    println!("{points}");
+    println!("{}", multi.iter().sum::<u32>());
 }
 
 
-fn matcher(lookup: &Vec<i32>, input: &Vec<i32>) -> Option<u32> {
+fn matcher(lookup: &Vec<i32>, input: &Vec<i32>) -> usize {
     let mut exp = 0;
     for number in input {
         if lookup.contains(number) {
             exp += 1;
         }
     }
-    if exp == 0 {
-        None
-    } else {
-        Some(exp - 1)
-    }
+    exp
 }
 
